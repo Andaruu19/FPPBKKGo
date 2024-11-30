e4efc778fdbd68028f6da64aec6ed92305975eb5
Andaruu19 <prelhanip@gmail.com>
2024-12-01 00:41:52 +0700

    fix: preload when movie fetching

--- internal/repositories/mysql/movie_repository.go
+++ internal/repositories/mysql/movie_repository.go
@@ -13,18 +13,21 @@ type MovieRepository struct {
 // FindByID fetches a movie by its ID
 func (mr *MovieRepository) GetByID(id uint) (*domain.Movie, error) {
     var movie domain.Movie
-    if err := mr.DB.First(&movie, id).Error; err != nil {
+    // Use Preload to load Genre and Actor relationships
+    if err := mr.DB.Preload("Genre").Preload("Actor").First(&movie, id).Error; err != nil {
         return nil, err
     }
     return &movie, nil
 }
 
+
 // FindAll fetches all movies
 func (mr *MovieRepository) GetAll() ([]domain.Movie, error) {
     var movies []domain.Movie
-    if err := mr.DB.Find(&movies).Error; err != nil {
+    if err := mr.DB.Preload("Genre").Preload("Actor").Find(&movies).Error; err != nil {
         return nil, err
     }
+
     return movies, nil
 }
 
