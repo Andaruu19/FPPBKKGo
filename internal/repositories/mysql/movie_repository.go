5a985052e2041cd5810e79eedcfbd6854d25bdcc
Andaruu19 <prelhanip@gmail.com>
2024-12-01 11:03:26 +0700

    fix: preloading and some endpoints

--- internal/repositories/mysql/movie_repository.go
+++ internal/repositories/mysql/movie_repository.go
@@ -13,7 +13,6 @@ type MovieRepository struct {
 // FindByID fetches a movie by its ID
 func (mr *MovieRepository) GetByID(id uint) (*domain.Movie, error) {
     var movie domain.Movie
-    // Use Preload to load Genre and Actor relationships
     if err := mr.DB.Preload("Genre").Preload("Actor").First(&movie, id).Error; err != nil {
         return nil, err
     }
@@ -34,7 +33,10 @@ func (mr *MovieRepository) GetAll() ([]domain.Movie, error) {
 // GetMoviesByAlbumID fetches all movies associated with a given album ID
 func (mr *MovieRepository) GetMoviesByAlbumID(albumID uint) ([]domain.Movie, error) {
 	var movies []domain.Movie
-	err := mr.DB.Joins("JOIN album_movies ON album_movies.movie_id = movies.id").
+	err := mr.DB.
+		Preload("Genre").
+		Preload("Actor").
+		Joins("JOIN album_movies ON album_movies.movie_id = movies.id").
 		Where("album_movies.album_id = ?", albumID).
 		Find(&movies).Error
 
